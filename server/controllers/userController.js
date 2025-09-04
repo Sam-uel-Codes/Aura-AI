5d1f4f24617f760dbef63af27cba33cc85f89e81
Sam-uel-Codes <samsuboff@gmail.com>
2025-09-04 17:16:43 +0530

    Finished the Front and Back end

---
+++ server/controllers/userController.js
@@ -0,0 +1,60 @@
+import sql from "../configs/db.js";
+
+export const getUserCreations = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+
+    const creations =
+      await sql`SELECT * FROM creations WHERE user_id = ${userId} ORDER BY created_at DESC`;
+
+    res.json({ success: true, creations });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getPublishedCrations = async (req, res) => {
+  try {
+    const creations =
+      await sql`SELECT * FROM creations WHERE publish = true ORDER BY created_at DESC`;
+
+    res.json({ success: true, creations });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const toggleLikeCreations = async (req, res) => {
+  try {
+
+    const {userId} = req.auth()
+    const {id} = req.body
+    
+    const [creation] = await sql`Select * from creations where id = ${id}`
+    if(!creation){
+        return res.json({success : false, message : "Creation not found"})
+    } 
+
+    const currentLikes = creations.likes;
+    const userIdStr = userId.toString()
+    let updatedLikes;
+    let message;
+
+    if(currentLikes.includes(userIdStr)){
+        updatedLikes = currentLikes.filter((user) => user !== userIdStr);
+        message = 'Creation Unliked'
+    }
+    else{
+        updatedLikes = [...currentLikes, userIdStr]
+        message = 'Creation Liked'
+    }
+
+    const formattedArray = `{${updatedLikes.json(',')}}`
+    await sql` UPDATE creations SET likes = ${fromattedArray} :: text[] where id = ${id}`
+
+    res.json({ success: true, message});
+    
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
 import {auth} from "../middlewares/auth.js"
-import { generateArticle } from "../controllers/aiController.js";
+import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject, resumeReview } from "../controllers/aiController.js";
+import {upload} from "../configs/multer.js"
 
 const aiRouter = express.Router();
 aiRouter.post('/generate-article', auth, generateArticle);
+aiRouter.post('/generate-blogtitles', auth, generateBlogTitle);
+aiRouter.post('/generate-image', upload.single('image'), auth, generateImage);
+aiRouter.post('/remove-background', upload.single('image'), auth, removeImageBackground);
+aiRouter.post('/remove-objects', upload.single('image'), auth, removeImageObject);
+aiRouter.post('/review-resume', upload.single('image'), auth, resumeReview);
 
 export default aiRouter
